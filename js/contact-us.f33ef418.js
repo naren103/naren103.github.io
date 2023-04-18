773fda97ef51d013551498779ea813eb106c029e
narenSD <narendra.softdevels.com>
2023-04-19 00:24:18 +0530

    first commit

---
+++ js/contact-us.f33ef418.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["contact-us"],{e9bb:function(t,a,s){"use strict";s.r(a);var n=s("62ad"),c=s("a523"),l=s("0fd9"),o=function(){var t=this,a=t._self._c;return a(l["a"],{attrs:{"no-gutters":""}},[a(n["a"],{attrs:{cols:"12"}},[a("p",{staticClass:"display-1"},[t._v("Contact Us")])]),a(c["a"],{attrs:{"grid-list-md":"",fluid:""}})],1)},r=[],u={name:"ContactUs"},e=u,i=s("2877"),d=Object(i["a"])(e,o,r,!1,null,null,null);a["default"]=d.exports}}]);
+//# sourceMappingURL=contact-us.f33ef418.js.map
